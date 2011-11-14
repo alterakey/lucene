e9c188719384b8ed1d1f9f5bb033f3c2a333fbcb
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2011-11-14 16:03:39 +0900

    Experimentally doing deltified transformation

--- src/com/gmail/altakey/lucene/LayerView.java
+++ src/com/gmail/altakey/lucene/LayerView.java
@@ -73,83 +73,54 @@ public class LayerView extends ImageView
 		return true;
 	}
 
-	private void focus(PointF focalPoint, float ratio)
+	private void focus(PointF focalPoint, float ratioDelta)
 	{
-		Matrix m = new Matrix();
-		m.setScale(ratio, ratio, focalPoint.x, focalPoint.y);
-		Log.d("LV.sS", String.format("scaling to: %f", ratio));
+		Matrix m = new Matrix(this.getImageMatrix());
+		m.postScale(1.0f + ratioDelta, 1.0f + ratioDelta, focalPoint.x, focalPoint.y);
 		this.setImageMatrix(m);
 	}
 
-	private void translate(float x, float y)
+	private void translate(float dx, float dy)
 	{
 		Matrix m = new Matrix(this.getImageMatrix());
-		m.setTranslate(x, y);
-		Log.d("LV.translate", String.format("translate: %f, %f", x, y));
+		m.postTranslate(dx, dy);
 		this.setImageMatrix(m);
 	}
 
 	private class PanController
 	{
-		private PointF origin = new PointF();
-		private PointF currentPoint = new PointF();
-
 		public void begin(MotionEvent e)
 		{
-			this.origin.x = e.getX();
-			this.origin.y = e.getY();
 		}
 
 		public void update(MotionEvent e)
 		{
-			if (this.origin.x == 0.0f && this.origin.y == 0.0f)
-				this.begin(e);
-
-			translate(e.getX() - this.origin.x + this.currentPoint.x, e.getY() - this.origin.y + this.currentPoint.y);
+			translate(e.getX() - e.getHistoricalX(0), e.getY() - e.getHistoricalY(0));
 		}
 
 		public void end()
 		{
-			this.origin.x = 0.0f;
-			this.origin.y = 0.0f;
-
-			float[] mv = new float[9];
-			getImageMatrix().getValues(mv);
-			this.currentPoint.x = mv[Matrix.MTRANS_X];
-			this.currentPoint.y = mv[Matrix.MTRANS_Y];
 		}
 	}
 
 	private class ZoomController
 	{
-		private float initial;
-		private float now;
-		private float currentRatio = 1.0f;
-
 		public void begin(ScaleGestureDetector sgd)
 		{
-			this.initial = sgd.getCurrentSpan();
-			this.now = 0.0f;
 		}
 
 		public void update(ScaleGestureDetector sgd)
 		{
-			this.now = sgd.getCurrentSpan();
-			if (this.initial == 0.0f)
-				this.initial = this.now;
+			float prev = sgd.getPreviousSpan();
+			float now = sgd.getCurrentSpan();
 
-			float ratio = this.now / this.initial * this.currentRatio;
+			float ratio = (now - prev) / prev;
+			Log.d("LV.ZC.update", String.format("ratio: %f", ratio));
 			focus(new PointF(sgd.getFocusX(), sgd.getFocusY()), ratio);
 		}
 
 		public void end()
 		{
-			this.initial = 0.0f;
-			this.now = 0.0f;
-
-			float[] mv = new float[9];
-			getImageMatrix().getValues(mv);
-			this.currentRatio = mv[Matrix.MSCALE_X];
 		}
 	}
 }
