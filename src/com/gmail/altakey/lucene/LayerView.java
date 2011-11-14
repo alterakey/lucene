90296675dc5e83c066e10be525345f32fbba09c1
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2011-11-14 09:32:58 +0900

    Using custom view in XML

---
+++ src/com/gmail/altakey/lucene/LayerView.java
@@ -0,0 +1,16 @@
+package com.gmail.altakey.lucene;
+
+import android.content.Context;
+import android.widget.ImageView;
+import android.util.AttributeSet;
+
+public class LayerView extends ImageView
+{
+	public LayerView(Context context) {
+		super(context);
+	}
+
+	public LayerView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+}
