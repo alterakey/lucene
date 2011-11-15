ffe70a18cb1040b625984eb6f0c543df9997f887
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2011-11-15 14:12:05 +0900

    Loading ads in main activity

--- src/com/gmail/altakey/lucene/MainActivity.java
+++ src/com/gmail/altakey/lucene/MainActivity.java
@@ -7,11 +7,23 @@ import android.widget.LinearLayout;
 
 public class MainActivity extends Activity
 {
+	private AdLoader adLoader;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+		this.adLoader = new AdLoader(this);
+		this.adLoader.load();
     }
+
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+		this.adLoader.load();
+	}
 }
