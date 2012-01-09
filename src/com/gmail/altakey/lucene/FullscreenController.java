6f9b4ca078e440b019683baafcf3e756083aa343
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2012-01-10 00:00:18 +0900

    Listening in more inner class

--- src/com/gmail/altakey/lucene/FullscreenController.java
+++ src/com/gmail/altakey/lucene/FullscreenController.java
@@ -16,7 +16,7 @@ public final class FullscreenController
 	private Timer restyleTimer;
 	private boolean active;
 
-	private final Styler styler = new Styler();
+	private final SystemUiStyler styler = new SystemUiStyler();
 
 	public FullscreenController(final View view)
 	{
@@ -46,7 +46,7 @@ public final class FullscreenController
 		{
 			this.activity.getActionBar().hide();
 			this.styler.styleSystemUi();
-			this.view.setOnSystemUiVisibilityChangeListener(this.styler);
+			this.styler.listen();
 		}
 		catch (NoSuchMethodError e)
 		{
@@ -55,61 +55,29 @@ public final class FullscreenController
 
 	public void deactivate()
 	{
-		if (!this.active)
-			return;
-
 		this.active = false;
 		this.activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		try
 		{
 			this.activity.getActionBar().show();
 			this.styler.styleSystemUi();
-			this.view.setOnSystemUiVisibilityChangeListener(null);
+			this.styler.unlisten();
 		}
 		catch (NoSuchMethodError e)
 		{
 		}
 	}
 
-	private final class Styler implements View.OnSystemUiVisibilityChangeListener
+	private final class SystemUiStyler
 	{
-		private final FullscreenController fsc = FullscreenController.this;
-		private Timer timer;
-
-		@Override
-		public void onSystemUiVisibilityChange(int visibility)
+		private boolean canHideSystemUi()
 		{
-			if (visibility != this.getExpectedVisibility())
-			{
-				if (this.timer != null)
-				{
-					this.timer.cancel();
-					this.timer.purge();
-					this.timer = null;
-				}
-				
-				this.timer = new Timer();
-				this.timer.schedule(new TimerTask() 
-				{
-					public void run()
-					{
-						activity.runOnUiThread(
-							new Runnable() 
-							{
-								public void run() 
-								{
-									styleSystemUi();
-								}
-							}
-						);
-					}
-				}, 1000);
-			}
+			return activity.getResources().getBoolean(R.bool.cap_can_hide_system_ui);
 		}
-
+		
 		private int getExpectedVisibility()
 		{
-			if (!fsc.active)
+			if (!active)
 			{
 				return View.SYSTEM_UI_FLAG_VISIBLE;
 			}
@@ -122,19 +90,75 @@ public final class FullscreenController
 			}
 		}
 
-		private boolean canHideSystemUi()
-		{
-			return fsc.activity.getResources().getBoolean(R.bool.cap_can_hide_system_ui);
-		}
-		
 		public void styleSystemUi()
 		{
 			try
 			{
-				fsc.view.setSystemUiVisibility(this.getExpectedVisibility());
+				view.setSystemUiVisibility(this.getExpectedVisibility());
 			}
 			catch (NoSuchMethodError e)
 			{
+			}
+		}
+
+		public void listen()
+		{
+			try
+			{
+				view.setOnSystemUiVisibilityChangeListener(new DeferredVisibilityRestorer());
+			}
+			catch (NoSuchMethodError e)
+			{
+			}
+			catch (NoClassDefFoundError e)
+			{
+			}
+		}	
+
+		public void unlisten()
+		{
+			try
+			{
+				view.setOnSystemUiVisibilityChangeListener(null);
+			}
+			catch (NoSuchMethodError e)
+			{
+			}
+		}
+
+		private class DeferredVisibilityRestorer implements View.OnSystemUiVisibilityChangeListener
+		{
+			private Timer timer;
+
+			@Override
+			public void onSystemUiVisibilityChange(int visibility)
+			{
+				if (visibility != getExpectedVisibility())
+				{
+					if (this.timer != null)
+					{
+						this.timer.cancel();
+						this.timer.purge();
+						this.timer = null;
+					}
+				
+					this.timer = new Timer();
+					this.timer.schedule(new TimerTask() 
+					{
+						public void run()
+						{
+							activity.runOnUiThread(
+								new Runnable() 
+								{
+									public void run() 
+									{
+										styleSystemUi();
+									}
+								}
+								);
+						}
+					}, 1000);
+				}
 			}
 		}
 	}
